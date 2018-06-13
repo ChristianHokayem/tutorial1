d51681e36c42387bab9d4aeda19b74658b102516
Christian Hokayem <christian.hokayem@ieee.org>
2018-06-13 20:47:05 +0300

    Buttons Added

--- Web-SpringBoot/src/main/java/com/local/expenses/controller/Main.java
+++ Web-SpringBoot/src/main/java/com/local/expenses/controller/Main.java
@@ -2,6 +2,8 @@ package com.local.expenses.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,7 +11,19 @@ public class Main {
     @RequestMapping("/")
     public String hello(Model model) {
         model.addAttribute("name", "toto");
+        model.addAttribute("text", "Hello, welcome to the site!");
+        return "main";
+    }
+
+    @RequestMapping(params = "action1", value = "action")
+    public String getButton(Model model) {
+        model.addAttribute("name", "tata");
+        return "main";
+    }
+
+    @RequestMapping(params = "action2", value = "action")
+    public String getbutton2(Model model) {
+        model.addAttribute("name", "titi");
         return "main";
     }
 }
-
--- Web-SpringBoot/src/main/webapp/WEB-INF/jsp/main.jsp
+++ Web-SpringBoot/src/main/webapp/WEB-INF/jsp/main.jsp
@@ -6,6 +6,14 @@
 
 <h1>"HI"</h1>
 <h2>${name}</h2>
+<h3>${text}</h3>
+
+<form action="action" >
+     <input type = "submit" value="a" name = "action1"/>
+     <input type = "submit" name = "action2"/>
+</form>
+
+
 </body>
 
 </html>
